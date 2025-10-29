7c47818106ddf1ac3cd8fc57dda9b221088b1502
AlejandroJorge <a20202060@pucp.edu.pe>
2025-10-29 18:25:21 -0500

    removed annoying -Wall flag on tests

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc $(find ./src -name "*.c" -not -name "main.c") ./test/*.c -Wall -o ./build/test
+gcc $(find ./src -name "*.c" -not -name "main.c") ./test/*.c -o ./build/test
 ./build/test
 rm ./build/test
