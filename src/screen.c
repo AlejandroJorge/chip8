13aa2b7b33e4b623263745acb77f8c48e2862d4d
AlejandroJorge <a20202060@pucp.edu.pe>
2025-10-30 23:10:41 -0500

    updated frequencies

--- src/screen.c
+++ src/screen.c
@@ -5,76 +5,119 @@
 
 #define SCALE 20
 #define FPS 60
-#define CPF 8
+#define CPU_HZ 600
+#define CPU_TIME_STEP (1.0f / CPU_HZ)
 
 void init_screen() {
-    const int screen_width = SW * SCALE;
-    const int screen_height = SH * SCALE;
+  const int screen_width = SW * SCALE;
+  const int screen_height = SH * SCALE;
 
-    InitWindow(screen_width, screen_height, "Chimpocho");
+  InitWindow(screen_width, screen_height, "Chimpocho");
 
-    SetTargetFPS(FPS);
+  SetTargetFPS(FPS);
 
-    Rectangle screen_pixels[SW*SH];
-    for(int i = 0; i < SW; i++) {
-        for(int j = 0; j < SH; j++) {
-            Rectangle r; r.x = i*SCALE; r.y = j*SCALE; r.height = SCALE; r.width = SCALE;
-            screen_pixels[i+j*SW] = r;
-        }
+  Rectangle screen_pixels[SW * SH];
+  for (int i = 0; i < SW; i++) {
+    for (int j = 0; j < SH; j++) {
+      Rectangle r;
+      r.x = i * SCALE;
+      r.y = j * SCALE;
+      r.height = SCALE;
+      r.width = SCALE;
+      screen_pixels[i + j * SW] = r;
     }
-    int h = 0;
-    while(!WindowShouldClose()) {
+  }
 
-        if(IsKeyDown(KEY_ONE)) keys_pressed[0] = true;
-        if(IsKeyDown(KEY_TWO)) keys_pressed[1] = true;
-        if(IsKeyDown(KEY_THREE)) keys_pressed[2] = true;
-        if(IsKeyDown(KEY_FOUR)) keys_pressed[3] = true;
-        if(IsKeyDown(KEY_Q)) keys_pressed[4] = true;
-        if(IsKeyDown(KEY_W)) keys_pressed[5] = true;
-        if(IsKeyDown(KEY_E)) keys_pressed[6] = true;
-        if(IsKeyDown(KEY_R)) keys_pressed[7] = true;
-        if(IsKeyDown(KEY_A)) keys_pressed[8] = true;
-        if(IsKeyDown(KEY_S)) keys_pressed[9] = true;
-        if(IsKeyDown(KEY_D)) keys_pressed[10] = true;
-        if(IsKeyDown(KEY_F)) keys_pressed[11] = true;
-        if(IsKeyDown(KEY_Z)) keys_pressed[12] = true;
-        if(IsKeyDown(KEY_X)) keys_pressed[13] = true;
-        if(IsKeyDown(KEY_C)) keys_pressed[14] = true;
-        if(IsKeyDown(KEY_V)) keys_pressed[15] = true;
+  float cum_time = 0.0f;
+  while (!WindowShouldClose()) {
+    cum_time += GetFrameTime();
 
-        if(IsKeyUp(KEY_ONE)) keys_pressed[0] = false;
-        if(IsKeyUp(KEY_TWO)) keys_pressed[1] = false;
-        if(IsKeyUp(KEY_THREE)) keys_pressed[2] = false;
-        if(IsKeyUp(KEY_FOUR)) keys_pressed[3] = false;
-        if(IsKeyUp(KEY_Q)) keys_pressed[4] = false;
-        if(IsKeyUp(KEY_W)) keys_pressed[5] = false;
-        if(IsKeyUp(KEY_E)) keys_pressed[6] = false;
-        if(IsKeyUp(KEY_R)) keys_pressed[7] = false;
-        if(IsKeyUp(KEY_A)) keys_pressed[8] = false;
-        if(IsKeyUp(KEY_S)) keys_pressed[9] = false;
-        if(IsKeyUp(KEY_D)) keys_pressed[10] = false;
-        if(IsKeyUp(KEY_F)) keys_pressed[11] = false;
-        if(IsKeyUp(KEY_Z)) keys_pressed[12] = false;
-        if(IsKeyUp(KEY_X)) keys_pressed[13] = false;
-        if(IsKeyUp(KEY_C)) keys_pressed[14] = false;
-        if(IsKeyUp(KEY_V)) keys_pressed[15] = false;
+    if (IsKeyDown(KEY_ONE))
+      keys_pressed[0] = true;
+    if (IsKeyDown(KEY_TWO))
+      keys_pressed[1] = true;
+    if (IsKeyDown(KEY_THREE))
+      keys_pressed[2] = true;
+    if (IsKeyDown(KEY_FOUR))
+      keys_pressed[3] = true;
+    if (IsKeyDown(KEY_Q))
+      keys_pressed[4] = true;
+    if (IsKeyDown(KEY_W))
+      keys_pressed[5] = true;
+    if (IsKeyDown(KEY_E))
+      keys_pressed[6] = true;
+    if (IsKeyDown(KEY_R))
+      keys_pressed[7] = true;
+    if (IsKeyDown(KEY_A))
+      keys_pressed[8] = true;
+    if (IsKeyDown(KEY_S))
+      keys_pressed[9] = true;
+    if (IsKeyDown(KEY_D))
+      keys_pressed[10] = true;
+    if (IsKeyDown(KEY_F))
+      keys_pressed[11] = true;
+    if (IsKeyDown(KEY_Z))
+      keys_pressed[12] = true;
+    if (IsKeyDown(KEY_X))
+      keys_pressed[13] = true;
+    if (IsKeyDown(KEY_C))
+      keys_pressed[14] = true;
+    if (IsKeyDown(KEY_V))
+      keys_pressed[15] = true;
 
-        if(h%10 == 0) {
-            BeginDrawing();
-            ClearBackground(BLACK);
-            for(int i = 0; i < SW*SH; i++) {
-                if(screen[i]) DrawRectangleRec(screen_pixels[i], GREEN);
-            }
-            EndDrawing();
-        }
+    if (IsKeyUp(KEY_ONE))
+      keys_pressed[0] = false;
+    if (IsKeyUp(KEY_TWO))
+      keys_pressed[1] = false;
+    if (IsKeyUp(KEY_THREE))
+      keys_pressed[2] = false;
+    if (IsKeyUp(KEY_FOUR))
+      keys_pressed[3] = false;
+    if (IsKeyUp(KEY_Q))
+      keys_pressed[4] = false;
+    if (IsKeyUp(KEY_W))
+      keys_pressed[5] = false;
+    if (IsKeyUp(KEY_E))
+      keys_pressed[6] = false;
+    if (IsKeyUp(KEY_R))
+      keys_pressed[7] = false;
+    if (IsKeyUp(KEY_A))
+      keys_pressed[8] = false;
+    if (IsKeyUp(KEY_S))
+      keys_pressed[9] = false;
+    if (IsKeyUp(KEY_D))
+      keys_pressed[10] = false;
+    if (IsKeyUp(KEY_F))
+      keys_pressed[11] = false;
+    if (IsKeyUp(KEY_Z))
+      keys_pressed[12] = false;
+    if (IsKeyUp(KEY_X))
+      keys_pressed[13] = false;
+    if (IsKeyUp(KEY_C))
+      keys_pressed[14] = false;
+    if (IsKeyUp(KEY_V))
+      keys_pressed[15] = false;
 
-        cpu_cycle();
-
-        if(delay_register > 0) delay_register--;
-        if(sound_register > 0) sound_register--;
-
-        h++;
+    while (cum_time >= CPU_TIME_STEP) {
+      cpu_cycle();
+      cum_time -= CPU_TIME_STEP;
     }
 
-    CloseWindow();
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    for (int i = 0; i < SW * SH; i++) {
+      if (screen[i])
+        DrawRectangleRec(screen_pixels[i], WHITE);
+    }
+
+    EndDrawing();
+
+    if (delay_register > 0)
+      delay_register--;
+    if (sound_register > 0)
+      sound_register--;
+  }
+
+  CloseWindow();
 }
